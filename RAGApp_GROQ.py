37566a4597016edf6c038ad4fb003549e1072161
Eligeti-13 <94038421+Eligeti-13@users.noreply.github.com>
2026-07-22 21:36:43 +0530

    Implement PDF upload and chat functionality

--- RAGApp_GROQ.py
+++ RAGApp_GROQ.py
@@ -2,9 +2,9 @@ import streamlit as st
 import tempfile
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from groq import Groq
 from sentence_transformers import SentenceTransformer, util
 import os
